bed0f4b343f7087aa4bb984d7e7d145d3d2bae90
Robbyro10 <63318506+Robbyro10@users.noreply.github.com>
2022-11-25 19:29:34 -0400

    first commit

---
+++ src/MovieTriviaApp.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+
+export const MovieTriviaApp = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  );
+};
