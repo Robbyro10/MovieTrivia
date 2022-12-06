7599ad9348c197b9b1b904ee3b6b0975187b757c
Paul Gamboa <paulgamboa.pgg@gmail.com>
2022-12-05 23:36:52 -0400

    Incorporacion de mas preguntas

--- src/data/questions.js
+++ src/data/questions.js
@@ -197,5 +197,260 @@ export const questions = [
             },
         ]
     },
+    {
+        'title': 'Who is the father of Simba (Lion King)?',
+        'category': 'characters',
+        'options': [
+            {
+                'title': 'Mundala',
+                'isCorrect': false
+            },
+
+            {
+                'title': 'Milan',
+                'isCorrect': false
+            },
+            {
+                'title': 'Mufasa',
+                'isCorrect': true
+            },
+            {
+                'title': 'Mandala',
+                'isCorrect': false
+            },
+        ]
+    },
+    {
+        'title': 'What color is Mike Wazowski?',
+        'category': 'characters',
+        'options': [
+            {
+                'title': 'Aqua',
+                'isCorrect': false
+            },
+
+            {
+                'title': 'Purple',
+                'isCorrect': false
+            },
+            {
+                'title': 'Blue',
+                'isCorrect': false
+            },
+            {
+                'title': 'Green',
+                'isCorrect': true
+            },
+        ]
+    },
+    {
+        'title': "What is the name of Negan's bat? (The Walking Dead)",
+        'category': 'characters',
+        'options': [
+            {
+                'title': 'Lucille',
+                'isCorrect': true
+            },
+            {
+                'title': 'Lucy',
+                'isCorrect': false
+            },
+
+            {
+                'title': 'Lili',
+                'isCorrect': false
+            },
+            {
+                'title': 'Camil',
+                'isCorrect': false
+            },
+
+        ]
+    },
+    {
+        'title': "What is the name of Negan's bat? (The Walking Dead)",
+        'category': 'characters',
+        'options': [
+            {
+                'title': 'Lucille',
+                'isCorrect': true
+            },
+            {
+                'title': 'Lucy',
+                'isCorrect': false
+            },
+
+            {
+                'title': 'Lili',
+                'isCorrect': false
+            },
+            {
+                'title': 'Camil',
+                'isCorrect': false
+            },
+
+        ]
+    },
+    {
+        'title': "Who is the Marvel hero who has a hammer?",
+        'category': 'characters',
+        'options': [
+            {
+                'title': 'Iron Man',
+                'isCorrect': false
+            },
+            {
+                'title': 'Thor',
+                'isCorrect': true
+            },
+            {
+                'title': 'Loki',
+                'isCorrect': false
+            },
+            {
+                'title': 'Black Widow',
+                'isCorrect': false
+            },
+
+        ]
+    },
+    {
+        'title': "Which country is Black Panther from?",
+        'category': 'characters',
+        'options': [
+            {
+                'title': 'South Africa',
+                'isCorrect': false
+            },
+            {
+                'title': 'Wakanda',
+                'isCorrect': true
+            },
+            {
+                'title': 'Somalia',
+                'isCorrect': false
+            },
+            {
+                'title': 'Tanzania',
+                'isCorrect': false
+            },
+
+        ]
+    },
+    {
+        'title': "Which district does Katniss Everdeen belong to? (The Hunger Games)",
+        'category': 'characters',
+        'options': [
+            {
+                'title': 'District 11',
+                'isCorrect': false
+            },
+            {
+                'title': 'District 121',
+                'isCorrect': false
+            },
+            {
+                'title': 'District 111',
+                'isCorrect': false
+            },
+            {
+                'title': 'District 12',
+                'isCorrect': true
+            },
+
+        ]
+    },
+    {
+        'title': "Which district does Katniss Everdeen belong to?",
+        'category': 'characters',
+        'options': [
+            {
+                'title': 'District 11',
+                'isCorrect': false
+            },
+            {
+                'title': 'District 121',
+                'isCorrect': false
+            },
+            {
+                'title': 'District 111',
+                'isCorrect': false
+            },
+            {
+                'title': 'District 12',
+                'isCorrect': true
+            },
+
+        ]
+    },
+    {
+        'title': "Who killed Glenn (The Walking Dead)?",
+        'category': 'characters',
+        'options': [
+            {
+                'title': 'A Zombie',
+                'isCorrect': false
+            },
+            {
+                'title': 'Negan',
+                'isCorrect': true
+            },
+            {
+                'title': 'Rick',
+                'isCorrect': false
+            },
+            {
+                'title': 'Carl',
+                'isCorrect': false
+            },
+
+        ]
+    },
+    {
+        'title': "How many dragons does Daenerys Targaryen have? (Game of Thrones)",
+        'category': 'characters',
+        'options': [
+            {
+                'title': '1',
+                'isCorrect': false
+            },
+            {
+                'title': '0',
+                'isCorrect': false
+            },
+            {
+                'title': '3',
+                'isCorrect': true
+            },
+            {
+                'title': '2',
+                'isCorrect': false
+            },
+
+        ]
+    },
+    {
+        'title': "Who was Daenerys's first husband",
+        'category': 'characters',
+        'options': [
+            {
+                'title': 'Khal Drogo',
+                'isCorrect': true
+            },
+            {
+                'title': 'Jon Snow',
+                'isCorrect': false
+            },
+            {
+                'title': 'Viserys Targaryen',
+                'isCorrect': false
+            },
+            {
+                'title': 'Aerys II Targaryen',
+                'isCorrect': false
+            },
+
+        ]
+    },
 
 ]
